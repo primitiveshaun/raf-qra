dce1c4f9a1e87aafd8ce1244aaefa31bb68cd1ce
Shaun Knowles <shaun@primitiveweb.dev>
2025-11-20 08:12:05 +0000

    feat: add explainer stepper

---
+++ src/app/qra/components/QRAStepper.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+
+import Step1 from "../qra-step1";
+import Step2 from "../qra-step2";
+import Step3 from "../qra-step3";
+
+// Define Props Interface
+interface QraStepperProps {
+    currentStep: number;
+    onAdvance: () => void;
+}
+
+export default function QraStepper({ currentStep, onAdvance }: QraStepperProps) {
+
+const steps = [
+    // Pass the onAdvance function to Step1
+    <Step1 key={0} onAdvance={onAdvance} />, 
+    <Step2 key={1} onAdvance={onAdvance} />, // It's good practice to pass it to all steps
+    <Step3 key={2} onAdvance={onAdvance} />,
+  ];
+
+  return (
+    <div className="relative h-full p-8">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentStep}
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -20 }}
+          transition={{ duration: 0.3 }}
+        >
+          {steps[currentStep]}
+        </motion.div>
+      </AnimatePresence>
+
+    </div>
+  );
+}
